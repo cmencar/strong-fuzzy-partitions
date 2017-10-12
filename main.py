328f771bd865dfb06d8ea496a4998864ee5ccd27
holydrinker <giuseppelorusso92@gmail.com>
2017-10-12 10:05:51 +0200

    add plot

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import strong_fuzzy_partition
+import sfp_plot
 
 cuts = [3, 8, 15, 23]
 min_max = [2, 25]
@@ -6,6 +7,9 @@ min_max = [2, 25]
 computed_trapezes = strong_fuzzy_partition.build_trapezes_series(cuts, min_max)
 res = [str(trapeze) for trapeze in computed_trapezes]
 
-print 'cuts: ' + str(cuts)
-print 'min-max: ' + str(min_max)
+print 'cuts:\n' + str(cuts)
+print 'min-max:\n' + str(min_max)
+print 'trapeze series:\n'
 print res
+
+sfp_plot.plot_trapeze_series(cuts, min_max, computed_trapezes, depth=5)
---
+++ sfp_plot.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+from data import TrapezoidalSFP
+
+
+def plot_trapeze_series(cuts, min_max, trapeze_series, depth):
+    _check_input(cuts, min_max, trapeze_series, depth)
+    _draw_axes(min_max, depth)
+    _draw_cuts(cuts)
+    for trapeze in trapeze_series:
+        _draw_trapeze(trapeze, depth, '-r')
+
+    plt.show()
+
+
+def _check_input(cuts, min_max, trapeze_series, depth):
+    assert isinstance(cuts, list), 'Cuts is not a list'
+    assert len(min_max) == 2, 'min_max param accepts only 2 values'
+    assert isinstance(trapeze_series, list), 'trapeze_series is not a list'
+    assert depth > 0, 'depth cant be less or equal than 0'
+
+
+def _draw_axes(min_max, depth):
+    min_x = min_max[0]
+    max_x = min_max[1]
+    min_y = -depth
+    max_y = (max_x - min_x) / 3
+
+    plt.axhline(y=0, color='k')
+    plt.axis([min_x, max_x, min_y, max_y])
+
+
+def _draw_cuts(cuts):
+    for cut in cuts:
+        plt.axvline(x=cut, linestyle='--')
+
+
+def _draw_trapeze(trapeze, depth, color):
+    assert isinstance(trapeze, TrapezoidalSFP)
+    plt.plot((trapeze.a, trapeze.b), (0, -depth), color)
+    plt.plot((trapeze.b, trapeze.c), (-depth, -depth), color)
+    plt.plot((trapeze.c, trapeze.d), (-depth, 0), color)
